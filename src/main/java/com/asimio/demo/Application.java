d03cb03d943513f643b75e26ce9f306644b258ba
Orlando L Otero <orlando.otero@asimio.net>
2020-04-07 11:45:37 -0400

    Enabling annotation-based transaction management

--- src/main/java/com/asimio/demo/Application.java
+++ src/main/java/com/asimio/demo/Application.java
@@ -3,9 +3,11 @@ package com.asimio.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = { "com.asimio.demo.dao" })
+@EnableTransactionManagement
 public class Application {
 
 	public static void main(String[] args) {
